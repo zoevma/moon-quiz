9271c7272b35573ffa0b8f14d25bcdfaeaaec7ce
zoe <zoe.marolf@gmail.com>
2017-12-16 12:00:53 +0100

    tried to print answer

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,14 @@
-window.alert("i work like magic");
+function check() {
+  window.alert("clicked");
+  
+  var a1 = document.getElementById("a1").checked;
+    var a3 = document.getElementById("a3").checked;
+
+    if (a1) {
+        document.getElementById("result").innerHTML = "you will get a rose as a gift";
+    } if else (a1 && a3) {
+        document.getElementById("result").innerHTML = "you will get a black rose as a gift";
+    }
+
+
+}
