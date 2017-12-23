24e2de066f6af60af00e2ca9055cac577553e958
zoe <zoe.marolf@gmail.com>
2017-12-23 14:11:53 +0100

    majed fixed everything

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,32 @@
 function check() {
-  window.alert("clicked");
-  
-  var a1 = document.getElementById("a1").checked;
-    var a3 = document.getElementById("a3").checked;
 
-    if (a1) {
-        document.getElementById("result").innerHTML = "you will get a rose as a gift";
-    } if else (a1 && a3) {
-        document.getElementById("result").innerHTML = "you will get a black rose as a gift";
-    }
+    console.log("try clicked");
+    var a1 = document.getElementsByName("color");
+    var b1 = document.getElementsByName("word");
+    var c1 = document.getElementsByName("dessert");
+    var result = 0;
+    a1.forEach(function(element) {
+      //  console.log(element);
+        if (element.checked){
+          result = result + Number(element.value) ;
+        }
+    });
 
+    b1.forEach(function(element) {
+      //  console.log(element);
+        if (element.checked){
+          result = result + Number(element.value) ;
+        }
+    });
+
+    c1.forEach(function(element) {
+      //  console.log(element);
+        if (element.checked){
+          result = result + Number(element.value);
+        }
+    });
+
+    //
+  console.log(result);
 
 }
