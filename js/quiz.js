94d05da9cdc68d00efc9439062a714b6b1c441c0
zoe <zoe.marolf@gmail.com>
2017-12-29 20:53:46 +0100

    responses tried and failed at quests

--- js/quiz.js
+++ js/quiz.js
@@ -5,6 +5,8 @@ function check() {
     var b1 = document.getElementsByName("word");
     var c1 = document.getElementsByName("dessert");
     var result = 0;
+    var output;
+
     a1.forEach(function(element) {
       //  console.log(element);
         if (element.checked){
@@ -26,7 +28,16 @@ function check() {
         }
     });
 
-    //
   console.log(result);
+    if (result > 19) {
+    output = "You will slowly become a hipster" ;
+    } else if (result > 10) {
+      output = "You will eat lots of your favorite food";
+    } else if (result > 0) {
+      output = "You will get a pet";
+    } else {
+      output = "You will visit your dream destination";
+    }
+document.getElementById("results").innerHTML = output;
 
 }
