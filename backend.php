f0f25879653706e7e14f683431abf7dcb943760a
zoe <zoe.marolf@gmail.com>
2018-02-03 12:29:49 +0100

    added backed.php

---
+++ backend.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_GET['name'];
+$password = $_GET['password'];
+$path = '/images/';
+if ($name == 'mark' && $password == 'kim') {
+$path = $path . 'cookies.jpg';
+$out_put = '<h1> Hello Newb</h1>';
+} else if ($name == 'zoe' && $password == 'star') {
+  $path = $path . 'candy.jpg';
+  $out_put = '<h1> Hello NSA</h1>';
+
+} else if ($name == 'majed' && $password == 'w00t') {
+  $path = $path . 'icecream.jpg';
+  $out_put = '<h1> Hello MAster</h1>';
+
+} else {
+  $path = $path . 'peace.jpg';
+  $out_put = '<h1 style="color=red"> Un authorized </h1>';
+}
+$out_put = $out_put . '<img src="'.$path.'" />';
+echo $out_put;
+ ?>
